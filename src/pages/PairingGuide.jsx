460aa0ec5333fc742cd5550f783a34486292b7da
kingdow10123 <80277412+kingdow10123@users.noreply.github.com>
2025-06-05 23:10:57 +0800

    Update PairingGuide.jsx

--- src/pages/PairingGuide.jsx
+++ src/pages/PairingGuide.jsx
@@ -6,7 +6,7 @@ export default function PairingGuide() {
 
   const checkIfReady = async () => {
     try {
-      const res = await fetch('https://ba7e3125be99.ngrok.app/get_state', {
+      const res = await fetch('https://7dad-211-72-73-210.ngrok-free.app/get_state', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ userId: '001' }),
